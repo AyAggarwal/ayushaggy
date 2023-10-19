ae850d7a4c982499d0fd571461d6cd6f303d43a6
Ayush Aggaral <ayushaggarwal@berkeley.edu>
2023-10-19 12:33:19 -0700

    animation and toggle

--- public/scripts.js
+++ public/scripts.js
@@ -11,24 +11,6 @@ document
     });
   });
 
-document
-  .getElementById("scrollDownButton2")
-  .addEventListener("click", function () {
-    // Get the position of the 'projects' section relative to the viewport.
-    let sectionPos = document
-      .getElementById("projects-title")
-      .getBoundingClientRect().top;
-
-    // Add the current scroll position to get the absolute position on the page.
-    let absolutePos = window.scrollY + sectionPos;
-
-    // Using the 'window' object's scroll method to scroll to the desired position.
-    window.scrollTo({
-      top: absolutePos,
-      behavior: "smooth", // Smooth scrolling.
-    });
-  });
-
 //hide button
 document
   .getElementById("toggleTikTokButton")
@@ -46,3 +28,58 @@ document
       buttonLabel.textContent = "Hide Video";
     }
   });
+
+document.getElementById("toggleTheme").addEventListener("click", function () {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  if (currentTheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+});
+
+if (!document.documentElement.hasAttribute("data-theme")) {
+  document.documentElement.setAttribute("data-theme", "dark"); // default to dark
+}
+
+// let scrollTimeout; // To store the timeout reference
+
+// document
+//   .getElementById("scrollDownButton2")
+//   .addEventListener("click", function () {
+//     // Get the position of the 'projects' section relative to the viewport.
+//     let sectionPos = document
+//       .getElementById("projects-title")
+//       .getBoundingClientRect().top;
+
+//     // Add the current scroll position to get the absolute position on the page.
+//     let absolutePos = window.scrollY + sectionPos;
+
+//     // Clear any previous timeouts (if any)
+//     clearTimeout(scrollTimeout);
+
+//     // Add an event listener for the scroll event
+//     window.addEventListener("scroll", onScroll);
+
+//     // Using the 'window' object's scroll method to scroll to the desired position.
+//     window.scrollTo({
+//       top: absolutePos,
+//       behavior: "smooth", // Smooth scrolling.
+//     });
+//   });
+
+// function onScroll() {
+//   // Clear any previous timeouts
+//   clearTimeout(scrollTimeout);
+
+//   // Set a new timeout
+//   scrollTimeout = setTimeout(function () {
+//     // This function will be called when the user stops scrolling for 66 milliseconds
+
+//     // Remove the event listener to avoid further detections
+//     window.removeEventListener("scroll", onScroll);
+
+//     // Hide the element
+//     document.getElementById("scrollDownButton2").style.display = "none";
+//   }, 66); // The delay, in milliseconds. Adjust as needed.
+// }
